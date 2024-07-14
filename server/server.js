d65a93508261ae77b6fd448edf46425fc541e87f
James Jelenko <jamesjelenko@gmail.com>
2024-07-14 02:02:35 -0700

    Add TODOs, some real, some not so real, it all depends on what gets done. Philosophy. Boom.

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ async function retrieveDescription(url) {
   }
 }
 
-
+// TODO: Add DB connection and check for retreived URL before running ^function^
+// TODO-2: Reach out and touch someone, a.k.a. make a computer at someone elses house use the retreived data to generate highly personal documents about moi xoxo
+// TODO-3: Use aforementioned DB connection to save URL and returned data from OAI
+// TODO-4: hide the evidence and rapidly look left-right-left-right reeaallllll shifty-like... yeah... yeah. Taht's da ticket. 
+// TODO-??? whistle innocently guys BE COOL GUYS BE COOL
 app.get('/url', (req, res) => {
   let url = req.params.target;
 
